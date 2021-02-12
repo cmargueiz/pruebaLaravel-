8e70ce23bcc004fdeca13578f2c19941ce2fd9e5
cmargueiz <c.margueiz@gmail.com>
2021-02-12 13:20:44 -0600

    arreglo de codigo de migraciones y seeder, configurando passport

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call([UsuarioSeeder::class,TwCorporativoSeeder::class]);
+        $this->call([UsuarioSeeder::class,CorporativoSeeder::class]);
     }
 }
